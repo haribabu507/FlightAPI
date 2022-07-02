f7ea3d85d0e7da4608fa1d8b3ff0073cfaeb4c98
Hari Sanniboyina <hari.sanniboyina@ing.com>
2022-07-02 11:11:20 +0200

    removed un used test method

--- src/test/java/com/flight/api/service/FlightServiceTest.java
+++ src/test/java/com/flight/api/service/FlightServiceTest.java
@@ -1,10 +1,8 @@
 package com.flight.api.service;
 
 import com.flight.api.entities.FlightData;
-import com.flight.api.exception.FlightAPICustomException;
 import com.flight.api.repository.FlightRepository;
 import com.flight.api.repository.FlightSortRepository;
-import com.flight.api.util.TestUtility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,7 +16,6 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.when;
 
 @DataJpaTest(showSql = true)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -41,16 +38,6 @@ public class FlightServiceTest {
     @DisplayName("Retrieving the flight details using mock repository")
     public void getFlightDetailsTest() {
         //when(flightRepositoryMock.searchAllByOriginOrDestination("AMS","DEL")).thenReturn(TestUtility.prepareListOfFlights());
-
-        List<FlightData> data = flightService.getFlightDetails("AMS", "DEL");
-        assertThat(data.size(), is(1));
-        assertThat(data.get(0).getFlightNumber(), is("A101"));
-    }
-
-    @Test
-    @DisplayName("Retrieving the flight details using mock repository")
-    public void getFlightDetailsExceptionTest() {
-        when(flightRepository.searchAllByOriginAndDestination("AMS","DEL")).thenThrow(new FlightAPICustomException("Failed to get flight info"));
 
         List<FlightData> data = flightService.getFlightDetails("AMS", "DEL");
         assertThat(data.size(), is(1));
